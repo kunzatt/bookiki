e7afeb1e25a6efb29a194360486b5d8d59362fc7
2Ludy <dkdl8412@gmail.com>
2025-01-17 17:46:08 +0900

    init: 백엔드 최초 생성

---
+++ Backend/src/main/java/com/corp/bookiki/BookikiApplication.java
@@ -0,0 +1,13 @@
+package com.corp.bookiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookikiApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/corp/bookiki/BookikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corp.bookiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
