bba6139569018f6a7ac4e07ca8a4122c8e066e51
kunzatt <kunzatt2501@gmail.com>
2025-02-03 12:17:52 +0900

    :art: style: JWTFilter import문 수정

--- Backend/src/main/java/com/corp/bookiki/jwt/filter/JWTFilter.java
+++ Backend/src/main/java/com/corp/bookiki/jwt/filter/JWTFilter.java
@@ -1,17 +1,10 @@
 package com.corp.bookiki.jwt.filter;
 
-import com.corp.bookiki.jwt.JwtTokenProvider;
-import com.corp.bookiki.jwt.service.JWTService;
-import com.corp.bookiki.user.adapter.SecurityUserAdapter;
-import com.corp.bookiki.util.CookieUtil;
-import io.jsonwebtoken.JwtException;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,10 +14,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import com.corp.bookiki.jwt.JwtTokenProvider;
+import com.corp.bookiki.jwt.service.JWTService;
+import com.corp.bookiki.user.adapter.SecurityUserAdapter;
+import com.corp.bookiki.util.CookieUtil;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
