7fb6bbf01505e29a5214e7c76873433d4302b9f8
2Ludy <dkdl8412@gmail.com>
2025-02-04 11:06:55 +0900

    :recycle: refactor: TestTokenController 로직 수정

--- Backend/src/main/java/com/corp/bookiki/jwt/controller/TestTokenController.java
+++ Backend/src/main/java/com/corp/bookiki/jwt/controller/TestTokenController.java
@@ -6,14 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.corp.bookiki.jwt.JwtTokenProvider;
-import com.corp.bookiki.user.entity.Role;
 import com.corp.bookiki.user.entity.UserEntity;
+import com.corp.bookiki.user.repository.UserRepository;
 import com.corp.bookiki.util.CookieUtil;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -31,22 +30,13 @@ public class TestTokenController {
 
 	private final JwtTokenProvider jwtTokenProvider;
 	private final CookieUtil cookieUtil;
+	private final UserRepository userRepository;
 
 	@Operation(summary = "사용자 테스트 토큰 발급", description = "일반 사용자 권한의 테스트용 JWT 토큰을 쿠키에 발급합니다.")
 	@GetMapping("/user")
 	public ResponseEntity<String> getUserToken(HttpServletResponse response) {
 		cleanupExistingTokens(response);
-
-		UserEntity testUser = UserEntity.builder()
-			.email("user-test@bookiki.com")
-			.userName("user-test")
-			.companyId("bookikiuser")
-			.role(Role.USER)
-			.deleted(false)
-			.build();
-
-		ReflectionTestUtils.setField(testUser, "id", 3);
-
+		UserEntity testUser = userRepository.findById(3).get();
 		issueTestTokens(response, testUser);
 		return ResponseEntity.ok("쿠키에 \"유저\" 권한 테스트 토큰이 발행되었습니다. (ID: 3)");
 	}
@@ -55,19 +45,9 @@ public class TestTokenController {
 	@GetMapping("/admin")
 	public ResponseEntity<String> getAdminToken(HttpServletResponse response) {
 		cleanupExistingTokens(response);
-
-		UserEntity testAdmin = UserEntity.builder()
-			.email("admin-test@bookiki.com")
-			.userName("admin-test")
-			.companyId("bookikiadmin")
-			.role(Role.ADMIN)
-			.deleted(false)
-			.build();
-
-		ReflectionTestUtils.setField(testAdmin, "id", 4);
-
+		UserEntity testAdmin = userRepository.findById(4).get();
 		issueTestTokens(response, testAdmin);
-		return ResponseEntity.ok("Admin test tokens issued to cookies (ID: 2)");
+		return ResponseEntity.ok("쿠키에 \"관리자\" 권한 테스트 토큰이 발행되었습니다. (ID: 4)");
 	}
 
 	private void issueTestTokens(HttpServletResponse response, UserEntity user) {
