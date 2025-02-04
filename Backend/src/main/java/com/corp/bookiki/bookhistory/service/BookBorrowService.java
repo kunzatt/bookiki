6695f0ee36f83b3bfb2d679219dd6f02f0990cc0
kunzatt <kunzatt2501@gmail.com>
2025-02-04 13:12:11 +0900

    :recycle: refactor: exception 수정

--- Backend/src/main/java/com/corp/bookiki/bookhistory/service/BookBorrowService.java
+++ Backend/src/main/java/com/corp/bookiki/bookhistory/service/BookBorrowService.java
@@ -1,16 +1,16 @@
-package com.corp.bookiki.bookcheckout.service;
+package com.corp.bookiki.bookhistory.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.corp.bookiki.bookcheckout.dto.BookBorrowRequest;
-import com.corp.bookiki.bookcheckout.dto.BookBorrowResponse;
-import com.corp.bookiki.bookcheckout.enitity.BookHistoryEntity;
-import com.corp.bookiki.bookcheckout.repository.BookCheckOutRepository;
+import com.corp.bookiki.bookhistory.dto.BookBorrowRequest;
+import com.corp.bookiki.bookhistory.dto.BookBorrowResponse;
+import com.corp.bookiki.bookhistory.enitity.BookHistoryEntity;
+import com.corp.bookiki.bookhistory.repository.BookHistoryRepository;
 import com.corp.bookiki.bookitem.entity.BookItemEntity;
 import com.corp.bookiki.bookitem.repository.BookItemRepository;
 import com.corp.bookiki.global.error.code.ErrorCode;
-import com.corp.bookiki.global.error.exception.BusinessException;
+import com.corp.bookiki.global.error.exception.BookHistoryException;
 import com.corp.bookiki.user.entity.UserEntity;
 import com.corp.bookiki.user.repository.UserRepository;
 
@@ -21,19 +21,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BookBorrowService {
 
-	private final BookCheckOutRepository bookHistoryRepository;
+	private final BookHistoryRepository bookHistoryRepository;
 	private final BookItemRepository bookItemRepository;
 	private final UserRepository userRepository;
 
 	public BookBorrowResponse borrowBook(BookBorrowRequest request) {
 		BookItemEntity bookItem = bookItemRepository.findById(request.getBookItemId())
-			.orElseThrow(() -> new BusinessException(ErrorCode.BOOK_ITEM_NOT_FOUND));
+			.orElseThrow(() -> new BookHistoryException(ErrorCode.BOOK_ITEM_NOT_FOUND));
 
 		UserEntity user = userRepository.findById(request.getUserId())
-			.orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+			.orElseThrow(() -> new BookHistoryException(ErrorCode.USER_NOT_FOUND));
 
 		if (!bookItem.isAvailable()) {
-			throw new BusinessException(ErrorCode.BOOK_ALREADY_BORROWED);
+			throw new BookHistoryException(ErrorCode.BOOK_ALREADY_BORROWED);
 		}
 
 		BookHistoryEntity history = new BookHistoryEntity(bookItem, user);
