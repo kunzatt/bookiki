3574fd0fca7769f24b7b2b29713675ee91da6950
이동욱 <dkdl8412@gmail.com>
2025-02-06 16:56:24 +0900

    RequestMapping 경로수정

--- Backend/src/main/java/com/corp/bookiki/user/controller/LogoutController.java
+++ Backend/src/main/java/com/corp/bookiki/user/controller/LogoutController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 @Tag(name = "인증 API", description = "사용자 로그인 및 인증 관련 API")
 public class LogoutController {
