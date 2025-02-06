2c12c128112fe081766951f1f6cb93f0cce2bcdf
kunzatt <kunzatt2501@gmail.com>
2025-02-06 13:20:54 +0900

    :sparkles: feat: book_informations에 있는 정보 book_items 테이블에 등록 api 구현 및 테스트 코드

--- Backend/src/main/java/com/corp/bookiki/bookitem/dto/BookItemRequest.java
+++ Backend/src/main/java/com/corp/bookiki/bookitem/dto/BookItemRequest.java
@@ -1,4 +1,20 @@
 package com.corp.bookiki.bookitem.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class BookItemRequest {
+	private Integer bookInformationId;
+	private LocalDateTime purchaseAt;
+
+	@Builder
+	public BookItemRequest(Integer bookInformationId, LocalDateTime purchaseAt) {
+		this.bookInformationId = bookInformationId;
+		this.purchaseAt = purchaseAt;
+	}
 }
