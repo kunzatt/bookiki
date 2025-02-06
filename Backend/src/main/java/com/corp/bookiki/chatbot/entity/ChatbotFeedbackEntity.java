b399734efa651ee587e0df6d56eaf861945d5053
yun_cha <yun090710@gmail.com>
2025-02-06 16:59:52 +0900

    [E011-S01] :recycle: refactor: entity 수정

--- Backend/src/main/java/com/corp/bookiki/chatbot/entity/ChatbotFeedbackEntity.java
+++ Backend/src/main/java/com/corp/bookiki/chatbot/entity/ChatbotFeedbackEntity.java
@@ -5,6 +5,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "chatbot_feedback")
@@ -31,6 +35,17 @@ public class ChatbotFeedbackEntity {
     @Column(name = "status", nullable = false)
     private FeedbackStatus status;
 
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false, columnDefinition = "TINYINT DEFAULT 0")
+    private boolean deleted; // false가 0으로, true가 1로 자동 변환
+
     @Builder
     public ChatbotFeedbackEntity(int userId, String originalIntent,
                                  String feedbackMessage, String category) {
@@ -39,6 +54,9 @@ public class ChatbotFeedbackEntity {
         this.feedbackMessage = feedbackMessage;
         this.category = category;
         this.status = FeedbackStatus.PENDING;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.deleted = false;
     }
 
     public void updateStatus(FeedbackStatus status) {
