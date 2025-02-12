747167863fc3a79c6d0cfa29ac40c4070718c423
2Ludy <dkdl8412@gmail.com>
2025-02-12 09:46:41 +0900

    ♻️ refactor: 비밀번호 정책 통일

--- Backend/src/main/java/com/corp/bookiki/user/dto/PasswordResetRequest.java
+++ Backend/src/main/java/com/corp/bookiki/user/dto/PasswordResetRequest.java
@@ -18,8 +18,8 @@ public class PasswordResetRequest {
 	)
 	@NotBlank(message = "새 비밀번호는 필수입니다.")
 	@Pattern(
-		regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
-		message = "비밀번호는 8자 이상이며, 영문자, 숫자, 특수문자를 포함해야 합니다."
+		regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
+		message = "비밀번호는 8~20자의 영문, 숫자, 특수문자를 포함해야 합니다"
 	)
 	private String newPassword;
 
