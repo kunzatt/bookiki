5862cc56caac4d97ff7633b60e24eae5ebb4df83
yuncof <yun090710@gmail.com>
2025-02-19 12:59:23 +0900

    :recycle: refactor: 뱃지 색상 변경, 오류 수정

--- Frontend/src/views/book/BookDetailView.vue
+++ Frontend/src/views/book/BookDetailView.vue
@@ -249,9 +249,10 @@ onUnmounted(() => {
               <BasicStatusBadge
                 v-if="$window.width >= 768"
                 :text="bookItem?.bookStatus === 'AVAILABLE' ? '대출 가능' : '대출 불가'"
-                :type="bookItem?.bookStatus === 'AVAILABLE' ? 'success' : 'error'"
-                size="M"
-                class="mb-4 transform scale-125 origin-left"
+                :type="bookItem?.bookStatus === 'AVAILABLE' ? success : 'error'"
+                size="S"
+                :isEnabled="false"
+                class="mb-4"
               />
 
               <div class="flex items-center gap-4 mb-4">
@@ -304,15 +305,16 @@ onUnmounted(() => {
 
       <!-- Mobile fixed bottom section -->
       <div class="md:hidden fixed bottom-[56px] left-0 right-0 bg-white border-t border-gray-200">
-        <div class="flex items-center justify-center h-[64px] px-4 mx-auto w-full min-w-[320px] max-w-[1280px]">
+        <div
+          class="flex items-center justify-center h-[64px] px-4 mx-auto w-full min-w-[320px] max-w-[1280px]"
+        >
           <div class="flex items-center gap-2 w-full max-w-[500px]">
             <div class="w-[130px]">
               <BasicStatusBadge
                 :text="bookItem?.bookStatus === 'AVAILABLE' ? '대출 가능' : '대출 불가'"
-                :type="bookItem?.bookStatus === 'AVAILABLE' ? 'primary' : 'error'"
-                size="M"
-                :isEnabled="true"
-                class="transform scale-125 origin-left"
+                :type="bookItem?.bookStatus === 'AVAILABLE' ? success : 'error'"
+                size="S"
+                :isEnabled="false"
               />
             </div>
             <BasicButton
